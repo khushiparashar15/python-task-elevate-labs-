07c1e2aa7f1e5f799c7526753ae75e04de78e068
khushiparashar15 <khushiparashar1504@gmail.com>
2026-01-29 19:03:19 +0530

    Add files via upload
    i have completed my first task of the python internship in which i learned python installation , file execution ,variables, input/output

---
+++ hello.py
@@ -0,0 +1,14 @@
+# enter name 
+name = input("Enter your name: ")
+
+# enter role of internship 
+role = input("Enter your internship role: ")
+
+# Enter today's date 
+date = input("Enter today's date (DD-MM-YYYY): ")
+
+# Print the details
+print("\n----- Intern Details -----")
+print("Name:", name)
+print("Role:", role)
+print("Date:", date)
